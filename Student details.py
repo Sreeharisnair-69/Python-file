5e3d6804590b21eb7ba533518ffbdf23c076a166
Sreeharisnair-69 <sreeharisreekumarnair2028@cs.sjcetpalai.ac.in>
2024-10-01 14:52:12 +0530

    My first commit

---
+++ Student details.py
@@ -0,0 +1,6 @@
+student_name = input("Enter name here:")
+roll_number = int (input("Enter roll number here:"))
+cgpa = float(input("Enter cgpa here:"))
+print("Your name is", student_name)
+print("Your roll number is", roll_number + 1)
+print("Your cgpa is", cgpa*10,"\b%")
