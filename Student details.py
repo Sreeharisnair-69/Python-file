8ae93bbcbd8c6eab8abdc50d65a2df72981ff0a2
Sreeharisnair-69 <sreeharisreekumarnair2028@cs.sjcetpalai.ac.in>
2024-10-01 15:25:50 +0530

    Add files via upload
    New information

--- Student details.py
+++ Student details.py
@@ -1,6 +1,14 @@
+''' Author: Sreehari Sreekumar Nair
+    Date: October 01, 2024
+    Purpose: To get an introduction to Python'''
+
 student_name = input("Enter name here:")
+age = int(input("Enter birth year here:"))
+program = input("Enter program here:")
 roll_number = int (input("Enter roll number here:"))
 cgpa = float(input("Enter cgpa here:"))
-print("Your name is", student_name)
-print("Your roll number is", roll_number + 1)
-print("Your cgpa is", cgpa*10,"\b%")
+print("Name:", student_name)
+print("Age:", 2024 - age)
+print("Program:",program)
+print("Roll number:", roll_number + 1)
+print("CGPA:", cgpa*10,"\b%")
