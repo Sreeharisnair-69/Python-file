fe395017e16883ea14151c31eecad2eef29158db
Sreeharisnair-69 <sreeharisreekumarnair2028@cs.sjcetpalai.ac.in>
2024-10-05 15:49:55 +0530

    Add files via upload

---
+++ Task 2: Concatenation of Strings.py
@@ -0,0 +1,11 @@
+'''
+Author: Sreehari Sreekumar Nair
+Date: 5 October 2024
+Purpose: To write a Python program that stores a string in a variable,
+         extract a specific part of the string (substring) and then
+         concatenate it with another string.
+'''
+str1 = str("Hello, World")
+str2 = str("Everyone!")
+str3 = str(str1[7] + str1[8] + str1[9] + str1[10] + str1[11])
+print(str2,str3)
