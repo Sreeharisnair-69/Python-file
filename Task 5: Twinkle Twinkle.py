f6adf91afc31a6fde433d13e06a4d100e3c61247
Sreeharisnair-69 <sreeharisreekumarnair2028@cs.sjcetpalai.ac.in>
2024-10-05 15:51:36 +0530

    Add files via upload

---
+++ Task 5: Twinkle Twinkle.py
@@ -0,0 +1,15 @@
+'''
+Author: Sreehari Sreekumar Nair
+Date: 5 October 2024
+Purpose: To write a Python program that prints the "Twinkle Twinkle" song.
+'''
+
+line_1 = str("Twinkle, twinkle, little star,")
+line_2 = str("How I wonder what you are!")
+line_3 = str("Up above the world so high,")
+line_4 = str("Like a diamond in the sky.")
+line_5 = str("Twinkle, twinkle, little star,")
+line_6 = str("How I wonder what you are")
+
+print(line_1,"\n\t\t",line_2,"\n\t\t\t\t",line_3,"\n\t\t\t\t",line_4,"\n",
+      line_5,"\n\t\t",line_6)
