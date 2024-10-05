6109dad11164766c7798dba45597053023c0d499
Sreeharisnair-69 <sreeharisreekumarnair2028@cs.sjcetpalai.ac.in>
2024-10-05 15:51:11 +0530

    Add files via upload

---
+++ Task 3: Usage of Operators.py
@@ -0,0 +1,18 @@
+'''
+Author: Sreehari Sreekumar Nair
+Date: 5 October 2024
+Pupose: To write a Python program that demonstrates the usage of arithmetic,
+        comparison, and logical operators.
+'''
+
+a = 10
+b = 5
+sum = a+b
+quotient = a/b
+
+
+print("Sum:",sum,"\b, Division:",quotient,"\n\nIs a greater than b?:",a>b,
+      "\n\nAre a and b equal?:",a==b,"\n\nLogical AND:",b<a and a!=b,
+      "\n\nLogical OR:",b!=b or a>b)
+
+
