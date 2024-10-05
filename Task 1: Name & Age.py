692a94185d6850a1e1dba18f41d735c9d51a2512
Sreeharisnair-69 <sreeharisreekumarnair2028@cs.sjcetpalai.ac.in>
2024-10-05 15:49:40 +0530

    Add files via upload

---
+++ Task 1: Name & Age.py
@@ -0,0 +1,8 @@
+'''
+Author: Sreehari Sreekumar Nair
+Date: 5 October 2024
+Purpose: To write a Python program that asks the user to input their name and age.
+'''
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+print("Hello,",name, "\b! You are", age, "years old.")
