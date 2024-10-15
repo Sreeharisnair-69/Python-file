0966433ccd2510701923d50cc369b71a007731cb
Sreeharisnair-69 <sreeharisreekumarnair2028@cs.sjcetpalai.ac.in>
2024-10-15 14:59:37 +0530

    Add files via upload

---
+++ Simple interest.py
@@ -0,0 +1,13 @@
+'''
+Name: Sreehari Sreekumar Nair
+Date: 15 October 2024
+Purpose: A program to calculate simple interest.
+'''
+
+p = float(input("Principal amount:"))
+n = float(input("Number of years:"))
+R = float(input("Rate:"))
+r = R/100
+
+simple_interest = p*n*r
+print(simple_interest)
