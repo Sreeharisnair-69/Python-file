90be42edc158c5223c6b77bae5c163da7263a276
Sreeharisnair-69 <sreeharisreekumarnair2028@cs.sjcetpalai.ac.in>
2024-10-15 14:58:15 +0530

    Add files via upload

---
+++ sign of a number.py
@@ -0,0 +1,16 @@
+'''
+Name: Sreehari Sreekumar Nair
+Date: 15 October 2024
+Purpose: A program to determine if the given number is positive or negative.
+'''
+
+num = float(input("Enter a number:"))
+
+if(num > 0):
+    print("Number is positive")
+
+elif(num == 0):
+    print("Number is zero")
+
+else:
+    print("Number is negative")
