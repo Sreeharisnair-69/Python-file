4e61e4e90548ae04d05bd9397f745f397a5b8957
Sreeharisnair-69 <sreeharisreekumarnair2028@cs.sjcetpalai.ac.in>
2024-10-15 16:21:22 +0530

    Add files via upload

---
+++ Sum of the digits of a number.py
@@ -0,0 +1,16 @@
+'''
+Name: Sreehari Sreekumar Nair
+Date: 15 October 2024
+Purpose: A program to find the sum of the digits of a number.
+'''
+
+num = int(input("Enter a number:"))
+
+sum = 0
+
+while(num > 0):
+    rem = num % 10
+    num = num // 10
+    sum = sum + rem
+
+print(sum)
