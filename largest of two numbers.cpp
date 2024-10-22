13c7154f1f9ab2c6a74b6392ba9fff3f50414c2d
Sreeharisnair-69 <sreeharisreekumarnair2028@cs.sjcetpalai.ac.in>
2024-10-22 15:59:10 +0530

    Add files via upload

---
+++ largest of two numbers.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+{
+   string raptor_prompt_variable_zzyz;
+   ?? number2;
+   ?? number1;
+
+   raptor_prompt_variable_zzyz ="""";
+   cout << raptor_prompt_variable_zzyz << endl;
+   cin >> number1;
+   raptor_prompt_variable_zzyz ="""";
+   cout << raptor_prompt_variable_zzyz << endl;
+   cin >> number2;
+   if (number1>number2)
+   {
+      cout << number1 << endl;   }
+   else
+   {
+      if (number1<number2)
+      {
+         cout << number2 << endl;      }
+      else
+      {
+         cout << "Both are equal" << endl;      }
+   }
+
+   return 0;
+}
