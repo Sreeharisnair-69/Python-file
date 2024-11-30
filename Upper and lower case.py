a889a9c95f7fd2050f19f2bc3dd8b7859311b7f5
Sreeharisnair-69 <sreeharisreekumarnair2028@cs.sjcetpalai.ac.in>
2024-11-30 10:43:53 +0530

    Update Upper and lower case.py

--- Upper and lower case.py
+++ Upper and lower case.py
@@ -1,3 +1,9 @@
+'''
+Name: Sreehari Sreekumar Nair
+Date: 30 November 2024
+Purpose: To calculate the number of upper case and lower case letters in a given string.
+'''
+
 def letters():
     string = input("Enter a string: ")
     upper_count = 0
